2b9c92b3bc56b51a1b1e4ddba65c8d59ae59edfb
Yuancheng Zhang <old.far.hot@gmail.com>
2017-08-01 23:56:51 -0500

    add validator and alert errors

--- routes/users.js
+++ routes/users.js
@@ -2,13 +2,42 @@ var express = require('express');
 var router = express.Router();
 
 // Register
-router.get('/register', function(req, res) {
+router.get('/register', function (req, res) {
 	res.render('register');
 });
 
 // Login
-router.get('/login', function(req, res) {
+router.get('/login', function (req, res) {
 	res.render('login');
+});
+
+// Register
+router.post('/register', function (req, res) {
+	var name = req.body.name;
+	var email = req.body.email;
+	var username = req.body.username;
+	var password = req.body.password;
+	var password2 = req.body.password2;
+
+	// Validation
+	req.checkBody('name', 'Name is required').notEmpty();
+	req.checkBody('email', 'Email is required').notEmpty();
+	req.checkBody('email', 'Email is not valid').isEmail();
+	req.checkBody('username', 'Username is required').notEmpty();
+	req.checkBody('password', 'Password is required').notEmpty();
+	req.checkBody('password2', 'Passwords do not match').equals(req.body.password);	
+	
+
+	// console.log(req.getValidationResult());
+	req.getValidationResult().then(function (result) {
+		if (!result.isEmpty()) {
+			res.render('register', {
+				errors: result.array()
+			});
+		} else {
+			console.log('PASSED');
+		}
+	});
 });
 
 module.exports = router;
